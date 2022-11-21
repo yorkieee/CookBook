13ae3199c35407b40d7bb5f14fd81dcb7fee7ed8
yorkieee <anastasiia.nahirniak@gmail.com>
2022-11-21 22:14:25 +0100

    passport library installation

---
+++ server/middleware/passport.js
@@ -0,0 +1,36 @@
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import * as dotenv from "dotenv";
+import passport from "passport";
+import client from "../dbClient.js";
+dotenv.config();
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET_KEY,
+};
+const jwtStrategy = new JwtStrategy(jwtOptions, async function (
+  jwt_payload,
+  done
+) {
+  try {
+    const res = await client.query(`SELECT * FROM users WHERE email = $1`, [
+      jwt_payload.email,
+    ]);
+    const user = res.rows[0];
+    console.log("user", user);
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+});
+
+export const passportConfig = () => {
+  passport.use(jwtStrategy);
+};
+
+export const jwtAuth = passport.authenticate("jwt", { session: false });
+export const oAuth = passport.authenticate("oauth", { session: false });
