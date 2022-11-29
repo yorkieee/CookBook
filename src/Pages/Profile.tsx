b218e5f854a63bb09b75017891f5297d8765d369
yorkieee <anastasiia.nahirniak@gmail.com>
2022-11-29 16:34:58 +0100

    profile frontend

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,13 +1,44 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Container } from "@mui/material";
+import { Box } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const Profile = () => {
   const { user } = useContext(AuthContext);
   console.log({ user });
+
   return (
-    <div className="profileCard">
-      <h1>{`Hey ${user?.user.name}`}</h1>
-      <div>{`Your email is ${user?.user.email}`}</div>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{ backgroundColor: "white", borderRadius: 5 }}
+    >
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      ></Box>
+      <div className="profileCard">
+        <h1>{`Welcome ${user?.user.name}`}</h1>
+        <div>{`Your email is ${user?.user.email}`}</div>
+        <Grid item>
+          <h1>
+            "Add your favourite recipes and share them with other cook
+            enthusiasts!"
+          </h1>
+          <Link style={{ textDecoration: "none" }} to="/newrecipe">
+            {" "}
+            Add a recipe
+          </Link>
+        </Grid>
+      </div>
+    </Container>
   );
 };
