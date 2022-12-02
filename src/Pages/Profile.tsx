d47f13f3a06f80095fcc6e0feeceef736236dba0
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-02 18:58:39 +0100

    fixed profile

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,16 +1,21 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { Container, Box, Button, Grid } from "@mui/material";
+import { Container, Box, Button } from "@mui/material";
 import { CssBaseline } from "@mui/material";
 import { Logout } from "../components/Logout";
 import { useNavigate } from "react-router-dom";
 import { Avatar } from "@mui/material";
+import { UpdateProfile } from "../components/UpdateUserName";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export const Profile = () => {
   const { user } = useContext(AuthContext);
-  console.log({ user });
-
   const navigate = useNavigate();
+
+  if (!user) {
+    navigate("/");
+    return null;
+  }
 
   return (
     <Container
@@ -27,33 +32,65 @@ export const Profile = () => {
           alignItems: "center",
         }}
       ></Box>
-      <Container>
-        <Grid container spacing={2}>
-          <Grid item>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-            <h1>{`Welcome ${user?.user.name}!`}</h1>
-            <Grid item>
-              <h1>{`Welcome ${user?.user.name}!`}</h1>
-              <p>{`Your email is ${user?.user.email}`}</p>
-              <Grid item>
-                <p>
-                  Add your favourite recipes and share them with other cook
-                  enthusiasts!
-                </p>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Container>
-      <Button
-        onClick={() => {
-          navigate(`/newrecipe`);
+
+      <Box
+        sx={{
+          marginTop: 4,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
         }}
       >
-        Add a recipe
-      </Button>
+        <UpdateProfile />
+        {/* <Avatar sx={{ width: 24, height: 24 }}>A</Avatar> */}
+        <Logout />
+      </Box>
+      <Box
+        sx={{
+          marginTop: 4,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <p>{`Welcome ${user?.user.name}!`}</p>
+        <p>{`Your email is ${user?.user.email}`}</p>
+      </Box>
 
-      <Logout />
+      <p>
+        Add your favourite recipes and share them with other cook enthusiasts!
+      </p>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <Button
+          onClick={() => {
+            navigate(`/newrecipe`);
+          }}
+        >
+          Add a recipe
+        </Button>
+        <Button
+          onClick={() => {
+            navigate(`/newrecipe`);
+          }}
+        >
+          {" "}
+          My recipes{" "}
+        </Button>
+        <Button
+          onClick={() => {
+            navigate(`/newrecipe`);
+          }}
+        >
+          {" "}
+          My <FavoriteIcon />{" "}
+        </Button>
+      </Box>
     </Container>
   );
 };
