18a7bef733a62e0a50475cdb9c9135b9e6c16894
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-04 18:25:11 +0100

    added controler to add comments to DB

--- server/controller/commentsController.js
+++ server/controller/commentsController.js
@@ -1,4 +1,5 @@
 import pool from "../dbConfig.js";
+import { dbGetUserNameByUid } from "../db-utils/dbGetUserNameByUid.js";
 
 const getCommentsByRecipeId = async (recipeID) => {
   const data = await pool.query(
@@ -7,7 +8,6 @@ const getCommentsByRecipeId = async (recipeID) => {
   );
 
   const comments = data.rows;
-  console.log("data", comments);
 
   return comments;
 };
@@ -18,6 +18,23 @@ export const getComments = async (req, res) => {
     const comments = await getCommentsByRecipeId(recipeId);
 
     res.json(comments);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const addComment = async (req, res) => {
+  const { comment, author_uid, recipe_id } = req.body;
+
+  try {
+    const author_name = await dbGetUserNameByUid(author_uid);
+
+    const data = await pool.query(
+      "INSERT INTO comments (comment, author_name, for_recipe_uid) VALUES ($1,$2,$3)",
+      [comment, author_name, recipe_id]
+    );
+
+    res.json(data.rows);
   } catch (err) {
     console.log(err.message);
   }
