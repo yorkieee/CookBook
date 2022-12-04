d28f90fd88b1695adaa5b66ca3d1ee0fdf887cd2
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-04 18:24:57 +0100

    added server route to add comments

--- server/routes/commentsRoutes.js
+++ server/routes/commentsRoutes.js
@@ -1,10 +1,10 @@
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import express from "express";
-import { getComments } from "../controller/commentsController.js";
+import { addComment, getComments } from "../controller/commentsController.js";
 
 const router = express.Router();
 
-// router.post("/addcomment", authMiddleware, addComment);
+router.post("/addcomment", authMiddleware, addComment);
 router.get("/getcomments", getComments);
 
 export default router;
