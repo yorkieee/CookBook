0c4e22e09ca58ae5605bc4c525ee416597759646
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-04 18:04:49 +0100

    rm empty lines

--- src/Pages/ListRecipes.tsx
+++ src/Pages/ListRecipes.tsx
@@ -2,27 +2,25 @@ import React from "react";
 import { Container } from "@mui/system";
 import { RecipeCard } from "../styles/RecipeCard";
 import { Grid } from "@mui/material";
-import {Layout} from "../styles/Layout"
+import { Layout } from "../styles/Layout";
 
 interface ListRecipesProps {
   recipeData: Array<string>;
 }
 
-
-
 const ListRecipes: React.FC<ListRecipesProps> = ({ recipeData }) => {
   return (
     <Layout>
-    <Container>
-      <h1> CookBook Recipes </h1>
-      <Grid container spacing={4}>
-        {recipeData.map((recipe, id) => (
-          <Grid item key={id} xs={12} md={3} lg={4}>
-            <RecipeCard recipe={recipe} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+      <Container>
+        <h1> CookBook Recipes </h1>
+        <Grid container spacing={4}>
+          {recipeData.map((recipe, id) => (
+            <Grid item key={id} xs={12} md={3} lg={4}>
+              <RecipeCard recipe={recipe} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </Layout>
   );
 };
