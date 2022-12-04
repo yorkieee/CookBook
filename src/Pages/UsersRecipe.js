3431d3faeca18bf74081463e72e6d623ca6e7f91
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-04 22:28:46 +0100

    UsersRecipe clean-up

--- src/Pages/UsersRecipe.js
+++ src/Pages/UsersRecipe.js
@@ -1,45 +1,14 @@
-import axios from "axios";
-import { useEffect, useState, useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-
-const backendUrl = "http://localhost:5001";
+import { useGetUserRecipes } from "../hooks/useGetUserRecipes";
+import { RecipeCard } from "../styles/RecipeCard";
 
 export const UsersRecipe = () => {
-  const [recipes, setRecipes] = useState([]);
-  const { user } = useContext(AuthContext);
-  const uid = user.user.uid;
-
-  const getRecipes = async () => {
-    try {
-      const options = {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-        },
-        method: "GET",
-      };
-      const data = await axios.get(
-        `${backendUrl}/usersrecipe?id=${uid}`,
-        options
-      );
-      if (data.data) {
-        console.log("recipes", data.data);
-        setRecipes(data.data);
-      }
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-
-  useEffect(() => {
-    getRecipes();
-  }, []);
+  const recipes = useGetUserRecipes();
 
   return (
     <div>
-      {recipes.map((recipe, id) => {
-        console.log(recipe);
-        return <h1 key={id}>{recipe.title}</h1>;
-      })}
+      {recipes.map((recipe, id) => (
+        <h1 key={id}>{recipe.title}</h1>
+      ))}
     </div>
   );
 };
---
+++ src/hooks/useGetUserRecipes.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const backendUrl = "http://localhost:5001";
+
+export const useGetUserRecipes = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const { user } = useContext(AuthContext);
+  const uid = user?.user.uid;
+
+  const getRecipes = async () => {
+    try {
+      const options = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+        method: "GET",
+      };
+      const data = await axios.get(
+        `${backendUrl}/usersrecipe?id=${uid}`,
+        options
+      );
+      if (data.data) {
+        console.log("recipes", data.data);
+        return data.data;
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const fetchedRecipes = await getRecipes();
+      setRecipes(fetchedRecipes);
+    };
+    fetch();
+  }, []);
+
+  return recipes;
+};
