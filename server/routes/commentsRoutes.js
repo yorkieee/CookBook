956931575f468abedd443ed162cb777c697f0ff5
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-04 16:54:22 +0100

    new comments route and connected controller

--- server/routes/commentsRoutes.js
+++ server/routes/commentsRoutes.js
@@ -1,9 +1,10 @@
-import { addComment } from "../controller/commentsController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import express from "express";
+import { getComments } from "../controller/commentsController.js";
 
 const router = express.Router();
 
-router.post("/addcomment", authMiddleware, addComment);
+// router.post("/addcomment", authMiddleware, addComment);
+router.get("/getcomments", getComments);
 
 export default router;
