c68e67d5735d0fd75341e721aef6fd2e0e94361f
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-04 19:13:14 +0100

    added post recipe comment FE to BE request

---
+++ src/backend-requests/postRecipeComment.ts
@@ -0,0 +1,16 @@
+export const postRecipeComment = async (comment: string, recipeId: string, authorId: string) => {
+    const options = {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({comment, author_uid: authorId, recipe_id: recipeId }),
+    };
+
+    await fetch(
+      'http://localhost:5001/addcomment',
+      options
+    );
+  };
+
