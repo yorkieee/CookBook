9f3173c06524876859c856169847b1fe9b606627
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-05 18:23:13 +0100

    fixed get favourites

--- server/controller/favouritesController.js
+++ server/controller/favouritesController.js
@@ -40,7 +40,7 @@ export const addFavourite = async (req, res) => {
 // };
 
 export const getFavouritesByUid = async (req, res) => {
-  const uid = req.body.uid;
+  const uid = req.query.uid;
   try {
     const data = await pool.query(
       `SELECT recipe_id, title, description, ingredients, description, instructions, favourites.id, favourites.user_id FROM recipes, favourites WHERE (recipes.uid = favourites.for_recipe_uid) AND (favourites.user_id = $1)`,
