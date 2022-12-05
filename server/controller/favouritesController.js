7c515af27574c5d4250f58611f61dde231e68871
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-05 18:39:05 +0100

    added delete fav BE

--- server/controller/favouritesController.js
+++ server/controller/favouritesController.js
@@ -15,29 +15,19 @@ export const addFavourite = async (req, res) => {
   }
 };
 
-// const getFavouritesByUid = async (uid) => {
-//   const user = await dbGetUserNameByUid(uid);
-//   const data = await pool.query(
-//     "SELECT recipe_id, title, description, ingredients, description,  favourites.id, favourites.author_name FROM recipes, favourites WHERE (recipes.uid = favourites.for_recipe_uid) AND (favourites.author_name = $1)"[
-//       user
-//     ]
-//   );
-//   const favourites = data.rows;
-//   console.log("response", data.rows);
+export const deleteFavourite = async (req, res) => {
+  try {
+    const { user_id, recipe_id } = req.body;
 
-//   return favourites;
-// };
-
-// export const getFavourites = async (req, res) => {
-//   const uid = req.body.uid;
-//   try {
-//     const recipes = await getFavouritesByUid(uid);
-
-//     res.json(recipes);
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// };
+    await pool.query(
+      `DELETE FROM favourites WHERE user_id = $1 AND for_recipe_uid=$2`,
+      [user_id, recipe_id]
+    );
+    res.json("favourite was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 export const getFavouritesByUid = async (req, res) => {
   const uid = req.query.uid;
@@ -45,8 +35,6 @@ export const getFavouritesByUid = async (req, res) => {
     const data = await pool.query(
       `SELECT recipe_id, title, description, ingredients, description, instructions, favourites.id, favourites.user_id FROM recipes, favourites WHERE (recipes.uid = favourites.for_recipe_uid) AND (favourites.user_id = $1)`,
       [uid]
-      // "SELECT * FROM favourites WHERE uid = $1, SELECT * FROM recipes WHERE recipe_id = $2",
-      // [uid, recipe_id]
     );
     res.json(data.rows);
   } catch (err) {
--- server/routes/favouritesRoutes.js
+++ server/routes/favouritesRoutes.js
@@ -2,12 +2,14 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import express from "express";
 import {
   addFavourite,
+  deleteFavourite,
   getFavouritesByUid,
 } from "../controller/favouritesController.js";
 
 const router = express.Router();
 
-router.post("/addfavourites", addFavourite);
-router.get("/favourites", getFavouritesByUid);
+router.post("/addfavourites", authMiddleware, addFavourite);
+router.get("/favourites", authMiddleware, getFavouritesByUid);
+router.delete("/deletefavourite", deleteFavourite);
 
 export default router;
