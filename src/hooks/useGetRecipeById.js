d11fcdfdfc61e1eece4ed987b9a8b2a43dc5e222
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-07 14:32:23 +0100

    added new hook for recipes by id

---
+++ src/hooks/useGetRecipeById.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const backendUrl = "http://localhost:5001";
+
+export const useGetRecipeById = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const { recipeId } = useParams();
+
+  const getRecipes = async () => {
+    try {
+      const options = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+        method: "GET",
+      };
+      const data = await axios.get(
+        `${backendUrl}/recipe/?id=${recipeId}`,
+        options
+      );
+      if (data.data) {
+        console.log("recipes", data.data);
+        return data.data;
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const fetchedRecipes = await getRecipes();
+      setRecipes(fetchedRecipes);
+    };
+    fetch();
+  }, []);
+
+  return recipes;
+};
