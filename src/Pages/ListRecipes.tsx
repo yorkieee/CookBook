8836475fed1775971069d1d53c0a1caaa9365276
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-08 14:38:06 +0100

    added is like on recipe list

--- src/Pages/ListRecipes.tsx
+++ src/Pages/ListRecipes.tsx
@@ -4,22 +4,32 @@ import { RecipeCard } from "../Pages/RecipeCard";
 import { Grid } from "@mui/material";
 import { Layout } from "../styles/Layout";
 import { SearchBar } from "../components/Search";
+import { useGetFavourites } from "../hooks/useGetFavourites";
 
-interface ListRecipesProps {
-  recipeData: Array<string>;
-}
-
-const ListRecipes: React.FC<ListRecipesProps> = ({ recipeData }) => {
+const ListRecipes: React.FC<any> = ({ recipeData }) => {
+  const favourites = useGetFavourites();
+  console.log({ favourites });
   return (
     <Layout>
       <Container>
         <SearchBar />
         <Grid container spacing={4}>
-          {recipeData.map((recipe, id) => (
-            <Grid item key={id} xs={12} md={3} lg={4}>
-              <RecipeCard recipe={recipe} />
-            </Grid>
-          ))}
+          {recipeData.map((recipe: any, id: string) => {
+            const recipeId = recipe.uid;
+            console.log({ recipeId });
+
+            const match = favourites.find(({ uid }: any) => {
+              return uid === recipeId;
+            });
+
+            const isLiked = !!match;
+
+            return (
+              <Grid item key={id} xs={12} md={3} lg={4}>
+                <RecipeCard isLiked={isLiked} recipe={recipe} />
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </Layout>
