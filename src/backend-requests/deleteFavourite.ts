f5ae1f4bed6d7a10888ebc69f0cf83428c0992c7
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-08 17:49:15 +0100

    added backend favourite delete util

---
+++ src/backend-requests/deleteFavourite.ts
@@ -0,0 +1,12 @@
+export const deleteFavourite = async (recipeId: string, userId: string) => {
+  const options = {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      "Content-Type": "application/json",
+    },
+    method: "DELETE",
+    body: JSON.stringify({ recipe_id: recipeId, user_id: userId }),
+  };
+
+  await fetch("http://localhost:5001/deletefavourite", options);
+};
