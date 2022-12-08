0210f40ad9a4f4c5d565b0ba4bb01e91aeb1c119
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-08 17:14:52 +0100

    formatting

--- src/backend-requests/postFavourite.ts
+++ src/backend-requests/postFavourite.ts
@@ -1,15 +1,12 @@
-export const postFavourite = async ( recipeId: string, userId: string) => {
-    const options = {
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("jwt")}`,
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({ recipe_id: recipeId, user_id: userId }),
-    };
-
-    await fetch(
-      'http://localhost:5001/addfavourites',
-      options
-    );
+export const postFavourite = async (recipeId: string, userId: string) => {
+  const options = {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ recipe_id: recipeId, user_id: userId }),
   };
+
+  await fetch("http://localhost:5001/addfavourites", options);
+};
