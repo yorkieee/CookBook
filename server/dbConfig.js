2dd2351a1faa9c5b5f0fa0608d67e31499dcf3b4
yorkieee <anastasiia.nahirniak@gmail.com>
2022-12-09 15:24:13 +0100

    vercel installed + new db

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -5,14 +5,20 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env;
+const connectionString = process.env.CONNECTION_STRING;
 
 const pool = new Pool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  database: DB_DATABASE,
-  port: DB_PORT,
+  connectionString,
 });
+
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env;
+
+// const pool = new Pool({
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   host: DB_HOST,
+//   database: DB_DATABASE,
+//   port: DB_PORT,
+// });
 
 export default pool;
